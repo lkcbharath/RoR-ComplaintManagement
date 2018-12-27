dc4d0ee3b150a3ae85f66fa359cd74423146620e
= <=>
2018-12-27 17:44:19 +0530

    Added scaffold and authentication using gems

---
+++ ComplaintManagement/db/migrate/20181227114637_create_complaints.rb
@@ -0,0 +1,11 @@
+class CreateComplaints < ActiveRecord::Migration[5.2]
+  def change
+    create_table :complaints do |t|
+      t.string :name
+      t.string :title
+      t.text :details
+
+      t.timestamps
+    end
+  end
+end
